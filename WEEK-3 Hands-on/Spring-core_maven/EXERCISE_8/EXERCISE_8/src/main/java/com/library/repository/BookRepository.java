331437289d36dfa657aa2ca297fb9b23498282ad
supraja-veerabathini <supraja.veerabathini@gmail.com>
2025-07-06 11:20:26 +0530

    Delete WEEK-3 Hands-on/Spring-core_maven/EXERCISE_8/EXERCISE_8 directory

--- WEEK-3 Hands-on/Spring-core_maven/EXERCISE_8/EXERCISE_8/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void save(String title) {
-        System.out.println("BookRepository: Saving book - " + title);
-    }
-}
